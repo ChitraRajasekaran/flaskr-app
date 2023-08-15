721244db5f977b5177b077bcaf674b309b35dc0c
Chitra Rajasekaran <crinspire01@gmail.com>
2023-08-14 22:50:26 -0400

    Updated trivia-app model and gitignore files

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
-from flask import Flask,jsonify
-from models import setup_db, Plant
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import random
+
+from models import setup_db, Question, Category
+
+QUESTIONS_PER_PAGE = 10
 
 def create_app(test_config=None):
     app = Flask(__name__)
